6bf5f636b09c1f5e547f6451bb01605bb186d196
Nick <nickmandev@gmail.com>
2017-06-11 22:38:38 +0300

    Feature: Implemented fetch of comments and their responses.

--- app/controllers/plex/comments_controller.rb
+++ app/controllers/plex/comments_controller.rb
@@ -5,11 +5,24 @@ module Plex
       @comment = Comment.new
     end
 
+    def show
+      puts params
+      response = []
+      comments = Comment.where(video_id: params[:id])
+      comments.each do |com|
+        obj = {}
+        obj['comment'] = com
+        com.comments_response ? obj['messages'] = com.comments_response : obj['messages'] = [];
+        response.push(obj)
+      end
+      render json: { comments: response }
+    end
+
     def create
       video = Video.find_by_id(comments_params['video_id'])
       comment = video.comments.new(comments_params)
       comment['user_info'] = UsersHelpers.current_user.name.to_json
-      comment.save ? (render json: { comment: comment }) : (render json: { error: 'The comment was not posted.' })
+      comment.save ? (render json: {comment: comment}) : (render json: {error: 'The comment was not posted.'})
     end
 
     def comments_params
